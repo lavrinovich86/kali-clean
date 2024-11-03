469a84366b0e2cc4c92ee827be0f0b59ed300bdd
ML <94226284+kali-i3@users.noreply.github.com>
2024-11-03 14:13:55 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 sudo apt update && sudo apt upgrade -y && sudo apt dist-upgrade -y
 
-sudo apt-get install -y wget curl git thunar htop mc
+sudo apt-get install -y wget curl git thunar htop mc meson ninja-build
 sudo apt-get install -y arandr flameshot arc-theme feh i3blocks i3status i3 i3-wm lxappearance python3-pip rofi unclutter cargo compton papirus-icon-theme imagemagick
 sudo apt-get install -y libxcb-shape0-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev autoconf meson
 sudo apt-get install -y libxcb-render-util0-dev libxcb-shape0-dev libxcb-xfixes0-dev 
@@ -17,18 +17,21 @@ unzip RobotoMono.zip -d ~/.local/share/fonts/
 
 fc-cache -fv
 
-wget https://github.com/barnumbirr/alacritty-debian/releases/download/v0.10.0-rc4-1/alacritty_0.10.0-rc4-1_amd64_bullseye.deb
-sudo dpkg -i alacritty_0.10.0-rc4-1_amd64_bullseye.deb
+#wget https://github.com/barnumbirr/alacritty-debian/releases/download/v0.10.0-rc4-1/alacritty_0.10.0-rc4-1_amd64_bullseye.deb
+#sudo dpkg -i alacritty_0.10.0-rc4-1_amd64_bullseye.deb
+#sudo apt install -f
+wget http://ftp.de.debian.org/debian/pool/main/r/rust-alacritty/alacritty_0.13.2-2+b3_amd64.deb
+sudo dpkg -i alacritty_0.13.2-2+b3_amd64.deb
 sudo apt install -f
 
-wget https://github.com/ninja-build/ninja/releases/download/v1.12.1/ninja-linux.zip
-unzip ninja-linux.zip
+#wget https://github.com/ninja-build/ninja/releases/download/v1.12.1/ninja-linux.zip
+#unzip ninja-linux.zip
 
 git clone https://www.github.com/Airblader/i3 i3-gaps
 cd i3-gaps && mkdir -p build && cd build && meson ..
 
 
-./ninja
+#./ninja
 #sudo ninja install
 cd ../..
 
