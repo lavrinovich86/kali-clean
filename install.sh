a8edb0d9140c0fcd5000f9e2c2373135bd41906e
ML <94226284+kali-i3@users.noreply.github.com>
2024-11-06 09:45:40 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sou
 
 sudo apt update && sudo apt upgrade -y && sudo apt dist-upgrade -y
 
-sudo apt-get install -y wget curl git thunar htop mc feroxbuster kali-desktop-i3-gaps batcat apt-transport-https sublime-text
+sudo apt-get install -y wget curl git thunar htop mc feroxbuster kali-desktop-i3-gaps batcat apt-transport-https sublime-text seclists kali-wallpapers-legacy kali-linux-default
 #meson ninja-build pkg-config
 sudo apt-get install -y arandr flameshot arc-theme feh i3blocks i3status i3 i3-wm lxappearance python3-pip rofi unclutter cargo compton papirus-icon-theme imagemagick
 sudo apt-get install -y libxcb-shape0-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev autoconf meson
@@ -60,6 +60,7 @@ echo "Done! Grab some wallpaper and run pywal -i filename to set your color sche
 echo "After reboot: Select i3 on login, run lxappearance and select arc-dark"
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sudo gzip -d /usr/share/wordlists/rockyou.txt.gz
 
 git clone https://github.com/Dewalt-arch/pimpmykali.git
 cd pimpmykali
