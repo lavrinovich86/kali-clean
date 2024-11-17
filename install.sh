d6fe53c8921f839a7f18cdf38f58d598b78430e6
ML <94226284+lavrinovich86@users.noreply.github.com>
2024-11-17 10:36:08 +0200

    Update install.sh
    A lot of changes

--- install.sh
+++ install.sh
@@ -1,19 +1,32 @@
 #!/bin/bash
 
+# add sublime-texteditor repositories
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 
 sudo apt update && sudo apt upgrade -y && sudo apt dist-upgrade -y
-sudo apt-get install -y wget curl git thunar htop mc feroxbuster kali-desktop-i3-gaps bat apt-transport-https sublime-text seclists kali-wallpapers-legacy kali-linux-default
-sudo apt-get install -y arandr flameshot arc-theme feh i3blocks i3status i3 i3-wm lxappearance python3-pip rofi unclutter cargo compton papirus-icon-theme imagemagick
-sudo apt-get install -y libxcb-shape0-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev autoconf meson
-sudo apt-get install -y libxcb-render-util0-dev libxcb-shape0-dev libxcb-xfixes0-dev 
+sudo apt-get install -y wget curl git thunar htop mc feroxbuster bat tree remmina
+#Sublime text-editor
+sudo apt install -y apt-transport-https sublime-text sublime-merge 
+#wordlists 
+sudo apt install -y seclists wordlists
+sudo apt install -y kali-wallpapers-all kali-linux-default
+sudo apt install -y arandr flameshot arc-theme feh kali-desktop-i3-gaps i3blocks i3status i3 i3-wm lxappearance python3-pip rofi unclutter cargo compton papirus-icon-theme imagemagick
+sudo apt install -y libxcb-shape0-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev autoconf meson
+sudo apt install -y libxcb-render-util0-dev libxcb-shape0-dev libxcb-xfixes0-dev 
 
+sudo apt install -y odat
+sudo apt install -y havoc
+
+sudo apt -y remove crackmapexec
+git clone https://github.com/Porchetta-Industries/CrackMapExec /opt/CrackMapExec
+cd /opt/CrackMapExec
+pipx install . --force
+
+# Fonsts install
 mkdir -p ~/.local/share/fonts/
-
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Iosevka.zip
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/RobotoMono.zip
-
 unzip Iosevka.zip -d ~/.local/share/fonts/
 unzip RobotoMono.zip -d ~/.local/share/fonts/
 
@@ -41,9 +54,23 @@ echo "Done! Grab some wallpaper and run pywal -i filename to set your color sche
 echo "After reboot: Select i3 on login, run lxappearance and select arc-dark"
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-sudo gzip -d /usr/share/wordlists/rockyou.txt.gz
+
+# Rockyou extract
+cd /usr/share/wordlists;
+sudo gzip -d /usr/share/wordlists/rockyou.txt.gz; cd ~
 
 git clone https://github.com/Dewalt-arch/pimpmykali.git
 cd pimpmykali
 #sudo ./pimpmykali.sh
 
+
+#Windows tools
+# psexec
+sudo mkdir /opt/psexec/; cd /opt/psexec/; sudo wget https://download.sysinternals.com/files/PSTools.zip; sudo unzip PSTools.zip; sudo rm PSTools.zip; cd ~;
+# accesschk
+sudo mkdir /opt/accesschk; cd /opt/accesschk; sudo wget https://download.sysinternals.com/files/AccessChk.zip; sudo unzip AccessChk.zip; sudo rm AccessChk.zip; cd ~;
+#NC
+mkdir /tmp/files; cd /tmp; wget https://eternallybored.org/misc/netcat/netcat-win32-1.12.zip; unzip netcat-win32-1.12.zip -d /tmp/files/; sudo mkdir /opt/nc; sudo cp /tmp/files/nc* /opt/nc/; sudo cp /usr/bin/nc /opt/nc/nc; cd ~;
+
+wget https://github.com/WerWolv/ImHex/releases/download/v1.35.4/imhex-1.35.4-Ubuntu-24.04-x86_64.deb
+sudo apt install ./imhex-*.deb
