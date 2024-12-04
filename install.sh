883e6aa5d57874db0914a95e4fe405b48b14a888
ML <94226284+lavrinovich86@users.noreply.github.com>
2024-12-04 12:42:42 +0200

    bug

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
 sudo echo "deb https://download.sublimetext.com/ apt/dev/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 
-sudo tee apt update && sudo apt upgrade -y && sudo apt dist-upgrade -y
+sudo apt update && sudo apt upgrade -y && sudo apt dist-upgrade -y
 sudo apt-get install -y wget curl git thunar htop mc feroxbuster bat tree remmina
 #Sublime text-editor
 sudo apt install -y apt-transport-https sublime-text sublime-merge 
